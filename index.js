9a68d250fa3f415ff19c4370de6ad4db4e53f3fc
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-17 21:42:31 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 5000
@@ -20,3 +23,20 @@ app.listen(port, () => {
 })
 
 //mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xplq2xb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const database = await client.db("mediCareDB");
+        console.log("database connected");
+        const doctorsCollection = database.collection('doctors');
+        
+
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir)
